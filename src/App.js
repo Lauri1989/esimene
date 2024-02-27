99cf443869642b0ef01d812ef19dd73b48c019f3
Lauri1989 <147425507+Lauri1989@users.noreply.github.com>
2024-02-27 19:40:27 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./Luuletus"
+import Luuletus from './/Luuletus';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Luuletus</h1>
+     <br />
+     <Luuletus />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Luuletus.js
@@ -0,0 +1,13 @@
+function Luuletus() {
+    return (
+        <div>
+            
+            <p>Kes hommikuti külas käib,</p>
+            <p>see asjatult ei longi</p>
+            <p>tidilipomm</p>
+            <p>sest selleks hommik ongi!</p>
+        </div>
+    )
+}
+
+export default Luuletus
